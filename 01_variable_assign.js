db4aed66a6fd821555917eebb63f2463b282029e
apurvakanade4 <apurvakanade4@gmail.com>
2023-05-24 12:19:43 +0530

    01_variable_assign.js
    01_variable_assign.js

---
+++ 01_variable_assign.js
@@ -0,0 +1,33 @@
+var fullName; // variable declaration
+console.log(fullName);
+
+var fullName = "Apurva Kanade"; // variable declaration
+console.log(fullName); // variable Initialization
+
+var fullName = "Apurva Sudhir Kanade";
+console.log(fullName);
+
+var myLove = "I Love only JS";
+console.log(myLove);
+
+var firstName = "Apurva";
+console.log(firstName);
+
+var lastName = "Kanade";
+console.log(lastName);
+
+var age;
+age = 27;
+console.log(age);
+
+var address = "At vishrantwadi pune";
+console.log(address);
+
+var pinCode = "411038";
+console.log(pinCode);
+
+var gender = "female";
+console.log(gender);
+
+var marritalStatus = "Married";
+console.log(marritalStatus);
---
+++ hello.js
@@ -0,0 +1,19 @@
+
+
+
+
+console.log("Apurva Kanade..");
+
+var firstName = "Apurva";
+console.log(firstName)
+
+var age;// Variable Declaration
+age = "22"; // Variable Initialization
+console.log(age);
+
+var holder // Variable Declaration
+console.log(holder); // undefined
+holder = "My Space"; // Variable Initialization
+
+holder = "Sachin Spec";// Variable Update
+console.log(holder);// Variable Initialization
