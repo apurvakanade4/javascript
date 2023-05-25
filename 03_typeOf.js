79a0c31c3143976219682c87074b58b2ec87f4b2
apurvakanade4 <apurvakanade4@gmail.com>
2023-05-25 15:11:26 +0530

    03_typeOf.js
    03_typeOf.js

---
+++ 03_typeOf.js
@@ -0,0 +1,101 @@
+var typesofjs = "--Before Initialization (variable value and type)--"
+console.log(typesofjs);
+var bankName = "Bank Name:=> Undefined"
+console.log(bankName);
+var bankType = "Bank Types:=> Undefined"
+console.log(bankType);
+
+console.log(" ");
+
+var typesofjs = "--After Initialization (variable value and type)--"
+console.log(typesofjs);
+var bankName = "Bank Name:=> SBI"
+console.log(bankName);
+var bankType = "Bank Types:=> String"
+console.log(bankType);
+console.log(" ");
+
+var Mark = "--Mark (variable value and type)--"
+console.log(Mark);
+var markType = "Mark Type:=> 90%"
+console.log(markType);
+var Type = "Type:=> String"
+console.log(Type);
+console.log(" ");
+
+var typesofjs = "--Before Initialization (variable value and type)--"
+console.log(typesofjs);
+var bankName = "isWorking:=> Undefined"
+console.log(bankName);
+var bankType = "Type:=> Undefined"
+console.log(bankType);
+console.log(" ");
+
+var type = "--After Initialization (variable value and type)--"
+console.log(type);
+var Name = "isWorking:=> true"
+console.log(Name);
+var bankType = "Type:=> String"
+console.log(bankType);
+console.log(" ");
+
+var count = "--Total Count (Variable value and type)--"
+console.log(count);
+var totalCount = "Total Count:=> Hundred and Seven"
+console.log(totalCount);
+var type = "Type:=> String"
+console.log(type);
+console.log(" ");
+
+var count = "--Total Count (Variable value and type)--"
+console.log(count);
+var totalCount = "Total Count:=> 107"
+console.log(totalCount);
+var type = "Type:=> Number"
+console.log(type);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
