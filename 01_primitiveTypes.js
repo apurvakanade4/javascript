63f09fbc91ca995e031de9374360a3de3538896e
apurvakanade4 <apurvakanade4@gmail.com>
2023-05-25 15:13:07 +0530

    02_variableBasicsAssig.js
    02_variableBasicsAssig.js

---
+++ 01_primitiveTypes.js
@@ -0,0 +1,52 @@
+// primitive Type
+// 1.Number
+var age = 31;
+var weight = 78;
+var height = 6.1;
+var speckNumber = 0.50;
+
+console.log(age, weight, height, speckNumber);
+var typeofAge = typeof age;
+console.log(typeofAge);
+
+console.log(speckNumber);
+
+console.log("Age", age);
+console.log("Weight", weight);
+console.log("Height", height);
+console.log("speckNumber", speckNumber);
+
+// 2. Boolean-->true or falser
+var isMarried = true;
+var knowAngular = true;
+var isSoftwareEngineer = true;
+var isBachelor = false;
+console.log(isBachelor);
+var typeofIsMarried = typeof isMarried;
+console.log("Typeof isMarried:" , typeofIsMarried);
+
+// 3. String --> Sequence of character enclosed in '' or ""
+var firstName = "Bill";
+var position = 'Microsoft co-founder';
+
+var typeofFirstName = typeof firstName;
+console.log("Type of variable -> firstName", typeofFirstName);
+console.log("Type of variable -> position", typeof position);
+
+console.log(2+2);
+
+//2. Special Data Type
+//1. Undefined
+var address;
+console.log("Address:", address);
+console.log("type of variable address is", typeof address);
+
+//2. null
+var pin = null;
+console.log("value of variable -> pin", pin);
+console.log('Type of variable pin is:', typeof pin);
+
+
+
+
+
---
+++ 02_variableBasicsAssig.js
@@ -0,0 +1,31 @@
+var dataTypes = "***Data Type Number***"
+console.log(dataTypes);
+var phoneNumber = phoneNumber
+console.log("Type of variable--> phoneNumber:", 9922330011);
+console.log("Type of variable--> phoneNumber is:",'Number');
+console.log(" ");
+
+var rollNumber = rollNumber
+console.log("Type of variable--> rollNumber:", 103);
+console.log("Type of variable--> rollNumber is:",'Number');
+console.log(" ");
+
+var dataTypes = "***Data Type String***"
+console.log(dataTypes);
+var profession = profession
+console.log("Type of variable--> profession:",'Software Engineer');
+console.log("Type of variable--> profession is:", 'string' );
+console.log(" ");
+
+var dataType = "**Data Type Boolean"
+console.log(dataTypes);
+var isMarried = true;
+console.log("Type of variable--> isMarried:", 'true');
+console.log("Type of variable--> isMarried:", 'Boolean');
+console.log(" ");
+
+var Difference = "**Difference between Undefined And Null**"
+console.log(Difference);
+console.log("Undefined means variable is decleared but its value is not assigned.");
+console.log("and Null means variable is declared but its value is assigned as null value");
+
